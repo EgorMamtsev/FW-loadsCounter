663e7921869db47d486ea4c6626d615c544173ea
Egor Mamtsev <egormamcev@MacBook-Air.local>
2025-06-11 13:52:56 -0500

    version 2. styles fixed, new function added, structure rebiulded

--- loadsCounter.js
+++ loadsCounter.js
@@ -1,168 +1,120 @@
+"use strict";
+
 // ==UserScript==
-// @name         Loads Counter
-// @namespace    http://tampermonkey.net/
-// @version      2024-05-06
-// @description  Count all loads on FW
-// @author       You
+// @name         NEW TEST COUNTER
+// @version      2.0
 // @match        https://erp.gologity.com/freight-watch/dispatch
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=gologity.com
-// @grant        none
-// ==/UserScript==
 
-(function () {
-  "use strict";
-
-  const p = new Promise((resolve, reject) => {
-    let interval = setInterval(() => {
-      const loadListAvailible = document.querySelector("tr.ng-star-inserted ");
-      if (loadListAvailible !== null) {
-        clearInterval(interval);
-        resolve();
-      }
-    }, 1000);
-  });
-
-  p.then(() => {
-    createColoredLoadsCounter();
-    setInterval(countRedLoads, 1000);
-    setInterval(countPinkLoads, 1000);
-    setInterval(countYellowLoads, 1000);
-    setInterval(countGreenLoads, 1000);
-    setInterval(countWhiteLoads, 1000);
-  });
-
-  function createColoredLoadsCounter() {
-    const elementAfterLoadsCounter = document.querySelector(
-      ".toggle-buttons-wrapper"
-    );
-    const searchField = document.querySelector(".search-wrapper");
-    const line = document.querySelector(".m-b-2");
-
-    const coloredCounter = document.createElement("div");
-    coloredCounter.classList.add("colored__loads__counter");
-    line.insertBefore(coloredCounter, elementAfterLoadsCounter);
-
-    const needTocheck = document.createElement("div");
-    needTocheck.classList.add("need__to__check");
-    coloredCounter.append(needTocheck);
-
-    const whiteLoads = document.createElement("div");
-    whiteLoads.classList.add("white__loads");
-    needTocheck.append(whiteLoads);
-
-    const whileLine = document.createElement("div");
-    whileLine.classList.add("white__line");
-    whileLine.textContent = "Need to check";
-    needTocheck.append(whileLine);
-
-    const eta = document.createElement("div");
-    eta.classList.add("eta");
-    coloredCounter.append(eta);
-    const pinkLoads = document.createElement("div");
-    pinkLoads.classList.add("pink__loads");
-    eta.append(pinkLoads);
-
-    const onSite = document.createElement("div");
-    onSite.classList.add("on__site");
-    coloredCounter.append(onSite);
-    const yellowLoads = document.createElement("div");
-    yellowLoads.classList.add("yellow__loads");
-    onSite.append(yellowLoads);
-
-    const inTransit = document.createElement("div");
-    inTransit.classList.add("in__transit");
-    coloredCounter.append(inTransit);
-    const greenLoads = document.createElement("div");
-    greenLoads.classList.add("green__loads");
-    inTransit.append(greenLoads);
-
-    const noAnswer = document.createElement("div");
-    noAnswer.classList.add("no__answer");
-    coloredCounter.append(noAnswer);
-    const redLoads = document.createElement("div");
-    redLoads.classList.add("red__loads");
-    noAnswer.append(redLoads);
-
-    const pinkLine = document.createElement("div");
-    pinkLine.classList.add("pink__line");
-    pinkLine.textContent = "ETA";
-    eta.append(pinkLine);
-
-    const yellowLine = document.createElement("div");
-    yellowLine.classList.add("yellow__line");
-    yellowLine.textContent = "On site";
-    onSite.append(yellowLine);
-
-    const greenLine = document.createElement("div");
-    greenLine.classList.add("green__line");
-    greenLine.textContent = "In transit";
-    inTransit.append(greenLine);
-
-    const redLine = document.createElement("div");
-    redLine.classList.add("red__line");
-    redLine.textContent = "N/A";
-    noAnswer.append(redLine);
-
-    const total = document.createElement("div");
-    total.classList.add("total");
-    coloredCounter.append(total);
-
-    const totalLoads = document.createElement("div");
-    totalLoads.classList.add("total__loads");
-    total.append(totalLoads);
-
-    const totalLine = document.createElement("div");
-    totalLine.classList.add("total__line");
-    totalLine.textContent = "Total";
-    total.append(totalLine);
+function pageLoadCheck() {
+  const loadListAvailible = document.querySelector("tr.ng-star-inserted");
+  if (loadListAvailible !== null) {
+    return true;
   }
+}
 
-  function countRedLoads() {
-    const redLoads = document.getElementsByClassName("red ng-star-inserted");
-    const redLoadsNumber = redLoads.length;
-    const numberOfRedLoads = document.querySelector(".red__loads");
-    numberOfRedLoads.textContent = redLoadsNumber;
-    return redLoadsNumber;
-  }
-  function countPinkLoads() {
-    const pinkLoads = document.getElementsByClassName("pink ng-star-inserted");
-    const pinkLoadsNumber = pinkLoads.length;
-    const numberOfPinkLoads = document.querySelector(".pink__loads");
-    numberOfPinkLoads.textContent = pinkLoadsNumber;
-    return pinkLoadsNumber;
-  }
+const counters = {};
 
-  function countYellowLoads() {
-    const yellowLoads = document.getElementsByClassName(
-      "orange ng-star-inserted"
-    );
-    const yellowLoadsNumber = yellowLoads.length;
-    let numberOfYellowLoads = document.querySelector(".yellow__loads");
-    numberOfYellowLoads.textContent = yellowLoadsNumber;
-    return yellowLoadsNumber;
-  }
-  function countGreenLoads() {
-    const greenLoads = document.getElementsByClassName(
-      "green ng-star-inserted"
-    );
-    const greenLoadsNumber = greenLoads.length;
-    const numberOfGreenLoads = document.querySelector(".green__loads");
-    numberOfGreenLoads.textContent = greenLoadsNumber;
-    return greenLoadsNumber;
-  }
-  function countWhiteLoads() {
-    const trElements = document.querySelectorAll("tr");
-    const trCount = trElements.length - 1;
-    const numberOfTotalLoads = document.querySelector(".total__loads");
-    numberOfTotalLoads.textContent = trCount;
-    const whiteLoadsNumber =
-      trCount -
-      (countRedLoads() +
-        countPinkLoads() +
-        countYellowLoads() +
-        countGreenLoads());
-    const numberOfWhiteLoads = document.querySelector(".white__loads");
-    numberOfWhiteLoads.textContent = whiteLoadsNumber;
-    return whiteLoadsNumber;
-  }
-})();
+function createLoadsCounter() {
+  const container = document.querySelector(".m-b-2");
+  const buttonsWrapper = document.querySelector(".toggle-buttons-wrapper");
+
+  const counter = document.createElement("div");
+  counter.classList.add("loads-counter");
+
+  container.insertBefore(counter, buttonsWrapper);
+
+  counters.needToCheck = createCounterItem("need-to-check", "Need to check");
+  counters.eta = createCounterItem("eta", "ETA");
+  counters.onSite = createCounterItem("on-site", "On site");
+  counters.inTransit = createCounterItem("in-transit", "In transit");
+  counters.noAnswer = createCounterItem("no-answer", "N/A");
+  counters.total = createCounterItem("total", "Total");
+
+  counter.append(
+    counters.needToCheck.element,
+    counters.eta.element,
+    counters.onSite.element,
+    counters.inTransit.element,
+    counters.noAnswer.element,
+    counters.total.element
+  );
+}
+
+function createCounterItem(modifier, labelText) {
+  const item = document.createElement("div");
+  item.classList.add("loads-counter__item", `loads-counter__item--${modifier}`);
+
+  const count = document.createElement("div");
+  count.classList.add("loads-counter__count");
+
+  const label = document.createElement("div");
+  label.classList.add("loads-counter__label");
+  label.textContent = labelText;
+
+  item.append(count, label);
+
+  return { element: item, countEl: count };
+}
+
+function countNoAnswer() {
+  const elems = document.getElementsByClassName("red ng-star-inserted");
+  const count = elems.length;
+  counters.noAnswer.countEl.textContent = count;
+  return count;
+}
+
+function countEta() {
+  const elems = document.getElementsByClassName("pink ng-star-inserted");
+  const count = elems.length;
+  counters.eta.countEl.textContent = count;
+  return count;
+}
+
+function countOnSite() {
+  const elems = document.getElementsByClassName("orange ng-star-inserted");
+  const count = elems.length;
+  counters.onSite.countEl.textContent = count;
+  return count;
+}
+
+function countInTransit() {
+  const elems = document.getElementsByClassName("green ng-star-inserted");
+  const count = elems.length;
+  counters.inTransit.countEl.textContent = count;
+  return count;
+}
+
+function countTotal() {
+  const trElements = document.querySelectorAll("tr");
+  const totalCount = trElements.length - 1; // якщо перший рядок - заголовок
+  counters.total.countEl.textContent = totalCount;
+  return totalCount;
+}
+
+function countNeedToCheck() {
+  const totalCount = countTotal();
+  const sumColored =
+    countNoAnswer() + countEta() + countOnSite() + countInTransit();
+  const needToCheckCount = totalCount - sumColored;
+  counters.needToCheck.countEl.textContent = needToCheckCount;
+  return needToCheckCount;
+}
+
+const pageLoadingCheck = new Promise((resolve, reject) => {
+  let interval = setInterval(() => {
+    if (pageLoadCheck()) {
+      clearInterval(interval);
+      resolve();
+    }
+  }, 1000);
+});
+
+pageLoadingCheck.then(() => {
+  createLoadsCounter();
+  setInterval(countNoAnswer, 1000);
+  setInterval(countEta, 1000);
+  setInterval(countOnSite, 1000);
+  setInterval(countInTransit, 1000);
+  setInterval(countNeedToCheck, 1000);
+  setInterval(countTotal, 1000);
+});
